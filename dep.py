f3aefecc90dee41f353448cb8c9b067c17298d59
hamzabenai <hamzabenai06@gmail.com>
2025-03-30 00:01:17 +0100

    adding requirements

--- dep.py
+++ dep.py
@@ -19,7 +19,9 @@ def predict(model, input_data):
 def main():
   st.title('This is a streamlit app for predicting the diamonds price:')
   st.write('This is a streamlit app for predicting the diamonds price:')
-  # we need first to link the streamlit with the github repo
+  st.write('here is the sample of the data :')
+  data = load_data()
+  st.write(data.head())
   
 if __name__ == '__main__':
   main()
